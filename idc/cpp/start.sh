f412caf598582abb7499ebabba3ba115ea96b93c
is-zq <1056180862@qq.com>
2025-02-03 18:36:37 +0800

    定期清理、压缩数据文件、日志文件

--- idc/cpp/start.sh
+++ idc/cpp/start.sh
@@ -5,3 +5,9 @@
 
 #生成气象站点观测数据，每分钟运行一次
 /project/tools/bin/procctl 60 /project/idc/bin/crtsurfdata /project/idc/ini/stcode.ini /tmp/idc/surfdata /log/idc/crtsurfdata.log csv,xml,json
+
+#定期清理旧的气象观测数据
+/project/tools/bin/procctl 300 /project/tools/bin/deletefiles /tmp/idc/surfdata "*" 0.02
+
+#定期压缩服务程序备份日志
+/project/tools/bin/procctl 300 /project/tools/bin/gzipfiles /log/idc "*.log.20*" 0.02
--- idc/cpp/stop.sh
+++ idc/cpp/stop.sh
@@ -5,10 +5,10 @@ killall -9 procctl
 
 #停止其他的服务程序
 #尝试让其他服务程序正常终止
-killall crtsurfdata
+killall crtsurfdata deletefiles gzipfiles
 
 #让其他服务程序有足够时间退出
 sleep 5
 
 #不管服务程序有没有退出，都强制杀死
-killall -9 crtsurfdata
+killall -9 crtsurfdata deletefiles gzipfiles
